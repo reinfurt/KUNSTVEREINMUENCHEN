f07b70070d81c04a26c42803072a3485f66bf360
RE User <re947454@m28s37da.ispgateway.de>
2017-06-19 17:22:03 +0200

    3. press changes complete

--- views/press.php
+++ views/press.php
@@ -5,9 +5,25 @@ $media_arr = $oo->media($uu->id);
 		<div class="images"><?
 			// foreach($media_arr as $media) {
 				// credits
-				?><div class="press-credits">
-					Please include credit Kunstverein München for all uses offline and online.
-				</div><?
+				?><div class="press-credits"><? 
+					if($lang == "en") {
+						?>
+For more information and images please contact us via <a href="mailto:presse@kunstverein-muenchen.de">email</a> or call +49 (0)89 22 11 52
+<br /><br />
+Please credit Künstverein Munchen e.V. in all reproductions offline and online and use the captions as they 
+appear underneath the images.
+						<?
+					} else {
+						?>
+Für weiteres Informations- und Bildmaterial wenden Sie sich bitte <a 
+href="mailto:presse@kunstverein-muenchen.de">per Mail</a> an unsere Pressestelle oder rufen Sie uns an unter +49 
+(0)89 22 11 52
+<br /><br />
+Die Verwendung des vorliegenden Bildmaterials ist nur im Zusammenhang mit einer Berichterstattung (print/online) 
+und nur unter Verwendung der entsprechenden Bildnachweise gestattet.
+						<?
+					}
+				?></div><?
 			// }
 			foreach($media_arr as $media)
 			{
